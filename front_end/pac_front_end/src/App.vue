e043c89ae66b2182726e89abd181fe3055f0c1fc
Valentīns Jermakovs <jermakovsvalentins@gmial.com>
2025-11-15 11:01:46 +0200

    Add a comment

--- front_end/pac_front_end/src/App.vue
+++ front_end/pac_front_end/src/App.vue
@@ -1,7 +1,9 @@
-<script setup></script>
-
 <template>
+  <!-- Šis fails ir ieejas punkts programmā -->
+  <!-- Tas atbild par PAC lapas renderu -->
   <h1 class="text-xl text-black italic">Hello PAC!</h1>
 </template>
+
+<script setup></script>
 
 <style scoped></style>
